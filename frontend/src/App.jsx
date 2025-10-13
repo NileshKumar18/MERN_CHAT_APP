274ecc8d5d434c6222dfc8f5723650cc5942a5f9
NieshKumar <nileshkumar200518@gmail.com>
2025-10-13 19:01:18 +0530

    set-up the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-2xl font-bold bg-red-50">Hello </div>
+    </>
+  )
+}
+
+export default App
